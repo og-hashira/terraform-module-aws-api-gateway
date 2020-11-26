06ae1b560d366f45f71ca0c4602e7d7d5ad994fd
Christopher Hesse <hesse.cd@pg.com>
2020-11-25 21:25:41 -0500

    foreach

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
-  value       = values(aws_api_gateway_rest_api.default)[*]["id"]
+  value       = values(aws_api_gateway_rest_api.default)[0]["id"]
   description = "The ID of the REST API."
 }
 
 output "execution_arn" {
-  value       = values(aws_api_gateway_rest_api.default)[*]["execution_arn"]
+  value       = values(aws_api_gateway_rest_api.default)[0]["execution_arn"]
   description = "The Execution ARN of the REST API."
 }
